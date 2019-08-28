b44f3d02244ffd6a18b8f47cb5079555cf9a902c
KhaMobile <KhaMobile@Sir-Face.wireless-1x.unc.edu>
2019-08-28 11:15:55 -0400

    the program works but has no commenting so far, really just a test

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,51 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int customers = scan.nextInt(); //number of customers
+		double[] totals = new double[customers]; //array of total value of purchases
+		String[] firstnames = new String[customers]; //array for name storage
+		String[] lastnames = new String[customers];
+		int items = 0; //declare number of item types variable
+		
+		for (int i = 0; i< customers; i++ )
+		{
+			//store name of customers
+			firstnames[i] = scan.next();
+			lastnames[i] = scan.next();
+			//store number of items he/she has w/ local variable
+			items = scan.nextInt();
+			//storage for each items totals, names of items
+			double[] totalperitem = new double[items];
+			String[] itemname = new String[items];
+			int quantity = 0;
+			
+			for (int j = 0; j< items; j++)
+			{
+				quantity = scan.nextInt();
+				itemname[j] = scan.next();
+				double costperitem = scan.nextDouble();
+				totalperitem[j] = quantity * costperitem;
+			}
+			 
+			totals[i] = calculateValueSum(totalperitem);
+			
+		}
+		for (int i = 0; i < customers; i++) {
+			System.out.println(firstnames[i].charAt(0)+". "+ lastnames[i] + ": " + String.format("%.2f", totals[i]) );
+		}
 		
 	}
+	
+	//sum array method
+	static double calculateValueSum(double[] vals) {
+		
+		double sum = 0;
+		
+		for (int i=0; i<vals.length; i++) {
+			sum += vals[i];
+		}
+		
+		return sum;
+	}
+	
 }
